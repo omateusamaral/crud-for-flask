8210d479377f564693cde0143ff4fdef676aa484
omateusamaral <mateus.amaral018@gmail.com>
2025-10-16 23:09:13 -0300

    refactor: rollback typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
+import { Product } from './entities/product.entity';
+import { join } from 'path';
 import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
@@ -7,6 +10,13 @@ import { CacheModule } from '@nestjs/cache-manager';
     CacheModule.register({
       isGlobal: true,
       ttl: 6 * 3600 * 1000, // 6 hours
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(__dirname, '..', 'data', 'db.sqlite'),
+      entities: [Product],
+      synchronize: true,
+      logging: false,
     }),
     ProductsModule,
   ],
